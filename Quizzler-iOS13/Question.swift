6c452639c6ddc1e2674a66568860903595f680c3
Tejasvni Dhir <tjdhir119@gmail.com>
2022-06-15 10:02:45 +0530

    Working quiz app

---
+++ Quizzler-iOS13/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  Quizzler-iOS13
+//
+//  Created by Tejasvni Dhir on 15/06/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct Question{
+    let q:String
+    let a:String
+}
